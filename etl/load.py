8062508e04e25338e56e4fa920ad414d4e22afd8
Edwin Abot <edwinabot@gmail.com>
2019-11-29 19:28:38 -0300

    CLI

--- etl/load.py
+++ etl/load.py
@@ -1,16 +1,12 @@
 import json
-import os
 import typing
 
 
-def dump_to_json_file(
-    data: typing.List[dict], destination_path: str, filename: str
-) -> str:
+def dump_to_json_file(data: typing.List[dict], destination_path: str) -> str:
     try:
-        path = os.path.join(destination_path, filename)
-        with open(path, mode="w") as output:
+        with open(destination_path, mode="w") as output:
             json.dump(data, output)
-        return path
+        return destination_path
     except Exception as ex:
         print(f"Error while dumping file {ex}")
         raise
--- job.py
+++ job.py
@@ -1,17 +1,45 @@
+import ast
+
+import click
+
 from etl.extract import extract_from_files, update_repo
 from etl.transform import retrieve_paths
 from etl.load import dump_to_json_file
 
-if __name__ == "__main__":
-    print("Hello TruSTAR!!")
-    repo_root = "/mnt/c/Users/edwin/Repos/cti"
-    files_path = "enterprise-attack/attack-pattern"
-    destination = "/mnt/c/Users/edwin/Repos/automatic-parakeet"
-    fields = ["id", "objects[0].name", "objects[0].kill_chain_phases"]
 
+class PythonLiteralOption(click.Argument):
+    def type_cast_value(self, ctx, value):
+        try:
+            ax = ast.literal_eval(value)
+            if not isinstance(ax, list):
+                raise
+            return ax
+        except Exception:
+            raise click.BadParameter(value)
+
+
+@click.command()
+@click.argument("repo_root")
+@click.argument("repo_files_path")
+@click.argument("output_destination")
+@click.argument("fields", cls=PythonLiteralOption)
+def run_job(repo_root, repo_files_path, output_destination, fields):
+    """
+    REPO_ROOT is the path on the filesystem for the git repo.
+
+    REPO_FILES_PATH is the path within the repo where the data sources exists
+
+    OUTPUT_DESTINATION is path for the output json file
+
+    FIELDS is the list of fields to extract from the sources
+    """
     update_repo(repo_root)
-    raw_data = extract_from_files(repo_root, files_path)
+    raw_data = extract_from_files(repo_root, repo_files_path)
     transformed_data = []
     for rd in raw_data:
         transformed_data.append(retrieve_paths(rd, fields))
-    dump_to_json_file(transformed_data, destination, "foo_edwin.json")
+    dump_to_json_file(transformed_data, output_destination)
+
+
+if __name__ == "__main__":
+    run_job()
